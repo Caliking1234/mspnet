8d36efab32d64f1265bffc59f4d614df0303e8b5
Piyush <piyyush1219@gmail.com>
2023-05-18 10:01:48 +0530

    commits

---
+++ src/components/HompageScreen/AnotherSection.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AnotherSection = () => {
+  return (
+    <div>
+      <div className=" w-2/3 text-center font-extrabold font-customised1 mx-auto">
+        <h1 className=" font-bold text-4xl md:text-6xl">
+          Improve and Innovate with the Tech Trends
+        </h1>
+        <p className=" my-2 text-2xl md:text-lg ">
+          We hire and build your own remote dedicated development teams tailored
+          to your specific needs.
+        </p>
+      </div>
+      <div>
+        <div>
+          <div>
+            <span></span>
+            <span></span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnotherSection;
