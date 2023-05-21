15623362b86310d77b78081a6843f9ea35ce7067
Piyush <piyyush1219@gmail.com>
2023-05-21 22:56:29 +0530

    emailjsdone

--- src/components/ContactUsPageScreen/Form.js
+++ src/components/ContactUsPageScreen/Form.js
@@ -1,10 +1,28 @@
 import React from "react";
+import emailjs from "emailjs-com";
 import ContacUsGif from "../../images/contactus.gif";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import EmailIcon from "@mui/icons-material/Email";
 import CallIcon from "@mui/icons-material/Call";
 
 const Form = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_um6i6oc",
+        "template_bax6i5h",
+        e.target,
+        "ANMhFipwRV0Ta3ePe"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="w-[95%] min-h-screen mx-auto flex justify-center items-center py-10 ">
       <div className=" w-[100%] py-5 bg-gray-200 sm:bg-white rounded-md flex flex-col sm:flex-row">
@@ -46,55 +64,66 @@ const Form = () => {
           <form
             action=""
             className=" w-[90%] sm:min-h-[100%] flex flex-col items-center justify-center gap-2 sm:gap-4"
+            onSubmit={sendEmail}
           >
             <input
               type="text"
               placeholder="Name"
+              name="name"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="email"
               placeholder="email"
+              name="user_email"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="number"
               placeholder="phone number"
+              name="user_contact"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="company"
+              name="user_company"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="position"
+              name="user_position"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="company's industry"
+              name="user_industry"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="number"
               placeholder="no of employees"
+              name="user_employeeno"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="country"
+              name="user_country"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="website"
+              name="user_website"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <input
               type="text"
               placeholder="type of query"
+              name="user_query"
               className=" ring mb-[3px] ring-white w-full text-xs  rounded-sm px-4 py-2 outline-none focus:ring-2 focus:ring-black"
             />
             <button className=" w-1/2 mt-3  py-2 px-5 text-white bg-cyan-700 hover:bg-white hover:text-cyan-700  hover:border-black hover:border-solid hover:border-[1px] ease-in duration-150 delay-75">
